4631e5e7c9b9a44ccb43761f2f20530f29e57bdf
GuyTeram <guyteram0@gmail.com>
2021-10-04 15:06:47 +0300

    Updates

---
+++ precache-manifest.43ea0e67267718af455a3166e8f37568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed0af3ebfe0b5433447eb069a5fc23a",
+    "url": "/SvShopProject/index.html"
+  },
+  {
+    "revision": "edc2fc02ff1b6a13af60",
+    "url": "/SvShopProject/static/css/main.815b5312.chunk.css"
+  },
+  {
+    "revision": "c70727dd55fa4c16ae48",
+    "url": "/SvShopProject/static/js/2.2e2f44ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SvShopProject/static/js/2.2e2f44ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc2fc02ff1b6a13af60",
+    "url": "/SvShopProject/static/js/main.c20189fe.chunk.js"
+  },
+  {
+    "revision": "0a50efe07dc78d6e7011",
+    "url": "/SvShopProject/static/js/runtime-main.235e2ccf.js"
+  }
+]);
